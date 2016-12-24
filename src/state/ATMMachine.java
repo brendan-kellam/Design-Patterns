d6899c870df80bccbf1058c404f57fd7be843030
brendan-kellam <bshizzle1234@gmail.com>
2016-12-24 14:37:39 -0500

    Added comments

--- src/state/ATMMachine.java
+++ src/state/ATMMachine.java
@@ -1,43 +1,73 @@
 package state;
 
 /**
- * -- The context --
+ * <h1> ATMMachine : Class </h1>
+ * ATMState atmState <br>
+ * ATMState hasCard  <br>
+ * ATMState noCard   <br>
+ * ATMState hasCorrectPin   <br>
+ * ATMState atmOutOfMoney   <br>
+ * int cashInMachine <br>
+ * boolean correctPinEntered <br>
+ * ---------------------------- <br>
+ * + setATMState(ATMState) : void <br>
+ * + setCashInMachine(int) : void <br>
+ * + insertCard() : void <br>
+ * + ejectCard() : void <br>
+ * + requestCash(int) : void <br>
+ * + insertPin(int) : void <br>
+ * 
+ * <p>
+ * Provides a context for all states of the ATMs
+ * </p>
+ * 
  **/
 
 public class ATMMachine {
 	
-	ATMState atmState;
 	
+	// --- define the different states ---
 	ATMState hasCard;
 	ATMState noCard;
 	ATMState hasCorrectPin;
 	ATMState atmOutOfMoney;
 	
-	int cashInMachine = 2000;
+	ATMState atmState; // current state of ATM
+
 	
-	// { defaults to false }
+	int cashInMachine = 2000;
 	boolean correctPinEntered = false;
 	
+	
+	// constructor 
 	public ATMMachine(){
+		
+		// initialize the states
 		hasCard = new HasCard(this);
 		noCard = new NoCard(this);
 		hasCorrectPin = new HasPin(this);
 		atmOutOfMoney = new NoCash(this);
 		
+		// set current state
 		atmState = noCard;
-	
+		
 		if (cashInMachine < 0) {
 			atmState = atmOutOfMoney;
 		}
 	}
 	
+	
+	// allows a given state to change the ATM's state
 	void setATMState(ATMState newATMState){
 		atmState = newATMState;
 	}
-	
+
 	public void setChashInMachine(int newCashInMachine){
 		cashInMachine = newCashInMachine;
 	}
+	
+	/* --- Define generalized methods for 3rd party usage ---
+	 * NOTE: Each method provides a abstracted gateway to the current ATMs state's corresponding method */
 	
 	public void insertCard(){
 		atmState.insertCard();
@@ -55,6 +85,7 @@ public class ATMMachine {
 		atmState.insertPin(pinEntered);
 	}
 
+	/* --- getters for each of the states --- */
 	public ATMState getYesCardState() { return hasCard; }
 	public ATMState getNoCardState() { return noCard; }
 	public ATMState getHasPin() { return hasCorrectPin; }
--- src/state/ATMState.java
+++ src/state/ATMState.java
@@ -1,5 +1,20 @@
 package state;
 
+/**
+ * <h1>ATMState : interface</h1>
+ * + insertCard() : void <br>
+ * + ejectCard() : void <br>
+ * + insertPin(int) : void <br>
+ * + rejectCash(int) : void
+ * <br>
+ * <p>
+ * These methods represent the <b>actions</b> a given user can perform on a ATM.<br>
+ * A given action may change the <b>state</b> of the ATM.<br>
+ * <b>NOTE: </b> every action must implement this interface and override these states
+ * </p>
+ * 
+ **/
+
 public interface ATMState {
 	
 	void insertCard();
